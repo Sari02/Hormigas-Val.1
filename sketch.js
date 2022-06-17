5c14e0f04cbb5d6a9047b2f0f5121cd028b66ea0
Sari02 <90795239+Sari02@users.noreply.github.com>
2022-06-16 20:01:03 -0500

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -5,7 +5,7 @@ var player1Img, player2Img, player3Img, player4Img; // para contener la imagen d
 var heart1, heart2, heart3;
 var heart1Img, heart2Img, heart3Img;
 
-
+var invisibleGround;
 
 
 
@@ -21,33 +21,37 @@ function preload(){
   player4Img = loadImage("assets/monoverde.png")
   
 
-  bgImg = loadImage("assets/bg1.png")
+  bgImg = loadImage("assets/bg.png")
 
 }
 
 function setup() {
 
   
-  createCanvas(windowWidth,windowHeight);
+  createCanvas(1300,600);
 
   //agregando la imagen de fondo 
-  bg = createSprite(displayWidth/2-20,displayHeight/2-40,20,20)
+  bg = createSprite(1300/2-20,600/2,20,20)
 bg.addImage(bgImg)
-bg.scale = 1.1
+bg.scale = 0.5
+
+// creando sprite de suelo
+invisibleGround = createSprite(1300/2,550,1300,10);
+invisibleGround.visible = true;
   
 
 //creando el sprite del jugador rojo
-player1 = createSprite(displayWidth-1150, displayHeight-300, 50, 50);
+player1 = createSprite(100,600-100, 50, 50);
  player1.addImage(player1Img)
-   player1.scale = 0.3
+   player1.scale = 0.5
    player1.debug = true
-   player1.setCollider("rectangle",0,0,300,300)
+   player1.setCollider("rectangle",0,0,200,200)
 
-   player2 = createSprite(displayWidth-1150, displayHeight-200, 50, 50);
+   player2 = createSprite(100,600-100, 50, 50);
    player2.addImage(player2Img)
-   player2.scale = 0.3
+   player2.scale = 0.5
    player2.debug = true
-   player2.setCollider("rectangle",0,0,300,300)
+   player2.setCollider("rectangle",0,0,200,200)
 
   //  player3 = createSprite(displayWidth-1150, displayHeight-300, 50, 50);
   //  player3.addImage(player3Img)
@@ -62,17 +66,17 @@ player1 = createSprite(displayWidth-1150, displayHeight-300, 50, 50);
   //  player4.setCollider("rectangle",0,0,300,300)
 
    //creando sprites para representar la vida sobrante
-   heart1 = createSprite(displayWidth-150,40,20,20)
+   heart1 = createSprite(1300-150,40,20,20)
    heart1.visible = false
     heart1.addImage("heart1",heart1Img)
     heart1.scale = 0.4
 
-    heart2 = createSprite(displayWidth-100,40,20,20)
+    heart2 = createSprite(1300-100,40,20,20)
     heart2.visible = false
     heart2.addImage("heart2",heart2Img)
     heart2.scale = 0.4
 
-    heart3 = createSprite(displayWidth-150,40,20,20)
+    heart3 = createSprite(1300-150,40,20,20)
     heart3.addImage("heart3",heart3Img)
     heart3.scale = 0.4
    
@@ -84,8 +88,9 @@ function draw() {
   background(0); 
 
   //moviendo al jugador arriba y abajo: volviéndolo compatible con juegos mobiles a traves de entrada táctil
-if((keyDown("W")||touches.length>0)&& player1.y > 130){
-  player1.y = player1.y-30
+if((keyDown("W")||touches.length>0)&& player1.y >= 100){
+  //player1.y = player1.y-30
+  player1.velocityY = -12;
   console.log(player1.x,player1.y);
 }
 if((keyDown("S")||touches.length>0)&& player1.y < 860){
@@ -105,7 +110,7 @@ if(keyDown("D")&& player1.x < 1660){
 
 
  //player2
- if(keyDown('UP_ARROW')||touches.length>0){
+ if((keyDown('UP_ARROW')||touches.length>0) && player1.y >= 100){
   player2.y = player2.y-30
 }
 if(keyDown('DOWN_ARROW')||touches.length>0){
@@ -120,6 +125,12 @@ if(keyDown('RIGHT_ARROW')){
   player2.x = player2.x-30
  }
 
+ // agregar gravedad
+ player1.velocityY = player1.velocityY + 0.8
+ player2.velocityY = player2.velocityY + 0.8
+
+ // hacer que los monos no se caigan
+ player1.collide(invisibleGround);
 
 //liberar las balas y cambiar la imagen del tirador a posición de disparo cuando la barra espaciadora es presionada 
 //if(keyWentDown("space")){
